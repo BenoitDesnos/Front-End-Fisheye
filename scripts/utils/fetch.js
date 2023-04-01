7c4b3fae077a587b2ad7e661c291ab8fc5672c7a
BenoitDesnos <103570316+BenoitDesnos@users.noreply.github.com>
2023-04-01 13:03:12 +0200

    delete fetch logs

--- scripts/utils/fetch.js
+++ scripts/utils/fetch.js
@@ -1,14 +1,12 @@
 async function getPhotographers() {
   const response = await fetch("../../data/photographers.json");
   const photographers = await response.json();
-  console.log(photographers);
   return photographers;
 }
 
 async function init() {
   // Récupère les datas des photographes
   const { photographers, media } = await getPhotographers();
-  console.log(photographers);
   switch (window.location.pathname) {
     case "/":
       displayHomeData(photographers);
@@ -16,7 +14,6 @@ async function init() {
     case "/photographer.html":
       displayPhotographerData(photographers, media);
       break;
-
     default:
       //displayError404
       break;
